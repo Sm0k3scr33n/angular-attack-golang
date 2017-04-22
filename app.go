25a9c3c00cfd39ca2a8c7365d42bd7aed16c1df7
michael gabbard <beng@michaels-MacBook-Pro.local>
2017-04-21 22:18:44 -0600

    added some handling for the URI stuff

--- app.go
+++ app.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "net/http"
-  //"log"
+  "log"
   //"fmt"
   "github.com/gorilla/mux"
   //"github.com/gorilla/securecookie"
@@ -10,12 +10,17 @@ import (
   //"gopkg.in/mgo.v2/bson"
 )
 
+// struct geoLoc{
+//   ID bson.ObjectId `bson:"_id,omitempty"`
+//   Lat string
+//   Long string
+// }
 
-func indexPageHandler(response http.ResponseWriter, request *http.Request) {
+func pageHandler404(response http.ResponseWriter, request *http.Request) {
 
 }
 
-func pageHandler404(response http.ResponseWriter, request *http.Request) {
+func indexPageHandler(response http.ResponseWriter, request *http.Request) {
 
 }
 
@@ -30,13 +35,22 @@ func challengeGet(response http.ResponseWriter, request *http.Request) {
 func uploadPhotoHandler(response http.ResponseWriter, request *http.Request) {
 
 }
+
 func voteHandler(response http.ResponseWriter, request *http.Request) {
 
 }
 
-
-
-
+func redirect(w http.ResponseWriter, req *http.Request) {
+    // remove/add not default ports from req.Host
+    target := "https://" + req.Host  + ":8000" +req.URL.Path
+    if len(req.URL.RawQuery) > 0 {
+        target += "?" + req.URL.RawQuery
+    }
+    log.Printf("redirect to: %s", target)
+    http.Redirect(w, req, target,
+            // see @andreiavrammsd comment: often 307 > 301
+            http.StatusTemporaryRedirect)
+}
 
 /// create a router with the gorilla mux router and handle the requests
 var router = mux.NewRouter()
@@ -48,7 +62,11 @@ func main() {
     router.HandleFunc("/challenge", challengeGet).Methods("GET")
     // router.HandleFunc("/location", Location).Methods("GET")
     router.HandleFunc("/uploadphoto", uploadPhotoHandler).Methods("POST")
-    router.HandleFunc("/vote", voteHandler).Methods("GET")
+    router.HandleFunc("/vote", voteHandler).Methods("POST")
     http.Handle("/", router)
+   port := ":80"
+     go http.ListenAndServe(port, http.HandlerFunc(redirect))
+     port2 := ":443"
+       go http.ListenAndServeTLS(port2,"cert.pem", "key.pem", http.HandlerFunc(redirect))
     http.ListenAndServeTLS(":8000", "cert.pem", "key.pem", nil)
 }
